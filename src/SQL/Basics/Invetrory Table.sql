4d40b704591da324c7e62650a8262b19e9050fb1
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-20 15:05:10 +0300

    final commit

--- src/SQL/Basics/Invetrory Table.sql
+++ src/SQL/Basics/Invetrory Table.sql
@@ -1,1 +1,9 @@
+--  SELECT inventory_id  FROM sakila.inventory;
+
+
 SELECT inventory_id  FROM sakila.inventory;
+--  SELECT inventory_id FROM  inventory WHERE inventory_id > 10
+SELECT * FROM sakila.inventory
+WHERE inventory_id IN (
+SELECT inventory_id FROM  inventory WHERE inventory_id > 10
+);
