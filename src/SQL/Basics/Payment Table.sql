c088145f7dba44b62aaa5741c2a31f01ea0ac76a
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-20 15:14:49 +0300

    final commit

--- src/SQL/Basics/Payment Table.sql
+++ src/SQL/Basics/Payment Table.sql
@@ -1,2 +1,8 @@
 -- rental_id
 SELECT rental_id  FROM sakila.payment;
+
+-- WHERE rental_id IN (SELECT rental_id FROM payment WHERE rental_id > 1000
+SELECT * FROM sakila.payment
+WHERE rental_id IN (SELECT rental_id FROM payment WHERE rental_id > 1000
+)
+;
