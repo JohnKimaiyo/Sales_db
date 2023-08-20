ae8f1b0caac751ab7a901f013b5c5eb6d988ec3f
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-20 16:30:42 +0300

    final commit

--- src/SQL/Basics/Customer Table.sql
+++ src/SQL/Basics/Customer Table.sql
@@ -6,9 +6,9 @@ SELECT  first_name FROM sakila.customer;
 -- SELECT address_id FROM customer WHERE address_id >30
 SELECT * FROM customer
 WHERE address_id IN
-(SELECT address_id FROM customer WHERE address_id >30)
+(SELECT address_id FROM customer WHERE address_id >30);
 
-
-
-
-;
+--  WHERE first_name LIKE 'Ma%'
+SELECT * FROM sakila.customer
+WHERE first_name LIKE 'Ma%'
+ORDER BY first_name,last_name DESC;
