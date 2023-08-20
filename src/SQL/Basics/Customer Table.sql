42e5e118abf7262e0f6469d3e0aac97ae168b564
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-20 14:05:16 +0300

    final commit

--- src/SQL/Basics/Customer Table.sql
+++ src/SQL/Basics/Customer Table.sql
@@ -1,3 +1,14 @@
 -- select first name
  
 SELECT  first_name FROM sakila.customer;
+
+
+-- SELECT address_id FROM customer WHERE address_id >30
+SELECT * FROM customer
+WHERE address_id IN
+(SELECT address_id FROM customer WHERE address_id >30)
+
+
+
+
+;
