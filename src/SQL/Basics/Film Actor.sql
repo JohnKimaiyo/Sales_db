17b68e87665d865c5a6f5256a966f929d9ba122f
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-20 14:34:07 +0300

    final commit

--- src/SQL/Basics/Film Actor.sql
+++ src/SQL/Basics/Film Actor.sql
@@ -1,3 +1,8 @@
 -- selct fil id
 
 SELECT  film_id  FROM sakila.film_actor;
+
+-- selct fil id
+SELECT * FROM sakila.film_actor
+WHERE film_id IN (SELECT film_id FROM film_actor WHERE film_id >300)
+;
