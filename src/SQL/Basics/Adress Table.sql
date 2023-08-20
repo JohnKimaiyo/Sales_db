6e86c548c793864a5b0729dce77d160e5ea85b9a
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-20 13:28:39 +0300

    final commit

--- src/SQL/Basics/Adress Table.sql
+++ src/SQL/Basics/Adress Table.sql
@@ -1,2 +1,7 @@
 --  select city id
 SELECT   city_id FROM sakila.address;
+
+-- SELECT city_id FROM address WHERE city_id >200
+SELECT * FROM address
+WHERE city_id in
+(SELECT city_id FROM address WHERE city_id >200  ) ;
