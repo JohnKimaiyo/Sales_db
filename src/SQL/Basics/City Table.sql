fe3f1696657500e05b81f4f87e993a59a2fde504
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-20 13:31:25 +0300

    final commit

--- src/SQL/Basics/City Table.sql
+++ src/SQL/Basics/City Table.sql
@@ -1,2 +1,9 @@
 -- select name
 SELECT  city FROM sakila.city;
+
+-- WHERE country_id in(SELECT country_id FROM city WHERE country_id >30)
+SELECT * FROM city
+WHERE country_id in(SELECT country_id FROM city WHERE country_id >30)
+
+
+;
