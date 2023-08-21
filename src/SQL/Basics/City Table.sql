08f0a8de727c40ff5d9a9bc5a9b9078aee698638
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-21 12:11:07 +0300

    inner join

--- src/SQL/Basics/City Table.sql
+++ src/SQL/Basics/City Table.sql
@@ -4,6 +4,10 @@ SELECT  city FROM sakila.city;
 -- WHERE country_id in(SELECT country_id FROM city WHERE country_id >30)
 SELECT * FROM city
 WHERE country_id in(SELECT country_id FROM city WHERE country_id >30)
-
-
 ;
+
+--INNER JOIN  city
+ON city.country_id = country.country_id;
+
+SELECT country.country,country.country_id,country.last_update FROM country INNER JOIN  city
+ON city.country_id = country.country_id;
