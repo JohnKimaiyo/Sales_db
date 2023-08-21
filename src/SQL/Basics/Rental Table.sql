b8c40371716c03eacba3c5aef6c156d136e22a5b
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-21 12:47:19 +0300

    final commit

--- src/SQL/Basics/Rental Table.sql
+++ src/SQL/Basics/Rental Table.sql
@@ -2,3 +2,12 @@
 -- WHERE customer_id IN ( SELECT customer_id FROM rental WHERE customer_id > 200);
 SELECT * FROM sakila.rental
 WHERE customer_id IN ( SELECT customer_id FROM rental WHERE customer_id > 200);
+
+-- BETWEEN 200 AND 1000
+SELECT
+rental_id, inventory_id,rental_id
+FROM 
+rental
+WHERE 
+inventory_id BETWEEN 200 AND 1000
+LIMIT 10;
