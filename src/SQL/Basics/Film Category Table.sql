d2f6782b60764eeab33e78d30cfc52f90fd0df76
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-08-21 12:53:46 +0300

    final commit

--- src/SQL/Basics/Film Category Table.sql
+++ src/SQL/Basics/Film Category Table.sql
@@ -3,3 +3,14 @@ SELECT * FROM sakila.film_category
 WHERE category_id IN(
 SELECT category_id film_category WHERE category_id > 10
 );
+
+
+--film_category
+SELECT
+film_id, category_id,last_update
+
+FROM
+film_category
+WHERE 
+category_id IN(6,11)
+LIMIT 10;
